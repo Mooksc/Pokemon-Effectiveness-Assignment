eb82ec8b742ad15f04e8def73f1c8ab36a155f45
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 11:15:11 -0600

    add returnDamageRelationsByTypeName method, taking a pokemon type as an argument and returing the "damage_relations" portion of the PokeApi 'type' endpoint query response

--- PokemonEffectiveness/PokeApiCalls.cs
+++ PokemonEffectiveness/PokeApiCalls.cs
@@ -10,5 +10,11 @@ namespace PokemonEffectiveness
             var response = await PokeApiGlobals.returnApiCall("pokemon/" + pokemonName);
             return response["types"][0]["type"]["name"].ToObject<string>();
         }
+
+        public static async Task<JToken> returnDamageRelationsByTypeName(string type)
+        {
+            var response = await PokeApiGlobals.returnApiCall("type/" + type);
+            return response["damage_relations"];
+        }
     }
 }
