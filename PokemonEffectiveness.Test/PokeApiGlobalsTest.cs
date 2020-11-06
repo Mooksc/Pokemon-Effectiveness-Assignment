d97e05a7002642a73c12b0da8f39fcb62d9af7a1
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 10:51:48 -0600

    add return type assertion unit test for returnApiCall method

---
+++ PokemonEffectiveness.Test/PokeApiGlobalsTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace PokemonEffectiveness.Test
+{
+    [TestClass]
+    public class PokeApiGlobalsTest
+    {
+        [DataTestMethod]
+        [DataRow("type/")]
+        [DataRow("pokemon/")]
+        public async Task Test_returnType_returnApiCall(string value)
+        {
+            var val = await PokemonEffectiveness.PokeApiGlobals.returnApiCall(value);
+            Assert.IsInstanceOfType(val, typeof(JObject), "Error - Expected JObject, got " + val.GetType());
+        }
+    }
+}
