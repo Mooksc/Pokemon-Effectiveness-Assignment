5251bd7d9e4b7ea86aa361cd47150655ae4144eb
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 11:05:13 -0600

    add return type assertion unit test for returnTypeByPokemonName method

---
+++ PokemonEffectiveness.Test/PokeApiCallsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace PokemonEffectiveness.Test
+{
+    [TestClass]
+    public class PokeApiCallsTest
+    {
+        [DataTestMethod]
+        [DataRow("charmander")]
+        [DataRow("koffing")]
+        [DataRow("steelix")]
+        public async Task Test_returnType_returnTypeByPokemonName(string value)
+        {
+            var val = await PokemonEffectiveness.PokeApiCalls.returnTypeByPokemonName(value);
+            Assert.IsInstanceOfType(val, typeof(string), "Error - Expected String, got " + val.GetType());
+        }
+    }
+}
