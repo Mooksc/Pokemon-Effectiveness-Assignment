43088e3056ddda58158dfe9bd88cba3a72a4415a
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 11:50:51 -0600

    implement PokeApiCalls and ApplicationInterface methods to Main program method

--- PokemonEffectiveness/Program.cs
+++ PokemonEffectiveness/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokemonEffectiveness
+﻿namespace PokemonEffectiveness
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Present title interface
+            ApplicationInterface.titleInterface();
+
+            // Store user query as variable
+            string query = ApplicationInterface.userQueryPrompt();
+
+            // Pass user query to returnTypeByPokemonName method
+            string pokemonType = PokeApiCalls.returnTypeByPokemonName(query).GetAwaiter().GetResult();
+
+            // Pass returnTypeByPokemonName return value to returnDamageRelationsByTypeName
+            var damage_relations = PokeApiCalls.returnDamageRelationsByTypeName(pokemonType).GetAwaiter().GetResult();
+
+            // Present query, pokemon type and damage_relation response to console
+            ApplicationInterface.presentResponse(query, pokemonType, damage_relations);
         }
     }
 }
