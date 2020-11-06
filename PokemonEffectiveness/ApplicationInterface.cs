f0613b9ab665c34b437a94442a74615a29890cf5
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 11:27:08 -0600

    add titleInterface method

--- PokemonEffectiveness/ApplicationInterface.cs
+++ PokemonEffectiveness/ApplicationInterface.cs
@@ -4,5 +4,10 @@ namespace PokemonEffectiveness
 {
     class ApplicationInterface
     {
+        public static void titleInterface()
+        {
+            Console.Write("--------------------------------------------" + "\n Pokemon Type Effectiveness Calculator\n" + "--------------------------------------------");
+            Console.Write("\nEnter the name of a Pokemon to discover is strengths and weaknesses!\n");
+        }
     }
 }
