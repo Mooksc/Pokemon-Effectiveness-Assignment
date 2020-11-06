c4031aba38b54ba204a2ea156aa46fec4a0552b3
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 11:01:15 -0600

    add returnTypeByPokemonName method, taking a pokemon name as an argument and returning a string of the type of pokemon

--- PokemonEffectiveness/PokeApiCalls.cs
+++ PokemonEffectiveness/PokeApiCalls.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace PokemonEffectiveness
 {
     public static class PokeApiCalls
     {
+        public static async Task<string> returnTypeByPokemonName(string pokemonName)
+        {
+            var response = await PokeApiGlobals.returnApiCall("pokemon/" + pokemonName);
+            return response["types"][0]["type"]["name"].ToObject<string>();
+        }
     }
 }
