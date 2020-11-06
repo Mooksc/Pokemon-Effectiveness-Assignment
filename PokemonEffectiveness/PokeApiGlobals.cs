4eaf0726767543537c0ffb2b5f2dd5dcbf71e32f
cmouk <cmouk@DESKTOP-P8P3C6L>
2020-11-06 10:43:55 -0600

    add asynchronous returnApiCall method which takes a PokeApi endpoint as an argument and returns the response as a deserialized JSON

--- PokemonEffectiveness/PokeApiGlobals.cs
+++ PokemonEffectiveness/PokeApiGlobals.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace PokemonEffectiveness
 {
@@ -7,5 +8,13 @@ namespace PokemonEffectiveness
     {
         public static readonly HttpClient httpClient = new HttpClient();
         public static string PokeApiURL = "https://pokeapi.co/api/v2/";
+
+        public static async Task<JObject> returnApiCall(string query)
+        {
+            var result = await httpClient.GetAsync(PokeApiURL + query);
+            string content = await result.Content.ReadAsStringAsync();
+            JObject joResponse = JObject.Parse(content);
+            return joResponse;
+        }
     }
 }
